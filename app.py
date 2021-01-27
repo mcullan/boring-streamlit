e435578d380931f3c7392d1217229359509337e6
Michael Cullan <mjcullan@gmail.com>
2021-01-27 16:58:19 -0500

    first commit

---
+++ app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+def app():
+    st.title("Nothing to see here")
+
+    st.markdown("""
+    ## Well... something to see here
+    * some bullet points
+    * nothing interesting
+
+    """)
+
+if __name__ == "__main__":
+    app()
